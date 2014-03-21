d20ae661a57dcafb2e9b2b3e5de9eb41621d2853
Gary Clayburg <gclaybur@comcast.net>
2014-03-21 11:37:11 -0600

    upgraded demo to work with Spring framework 4.0, Spring data 1.4.1 and Junit 4.11

--- src/test/java/com/johnathanmarksmith/mongodb/example/repository/PersonRepositoryTest.java
+++ src/test/java/com/johnathanmarksmith/mongodb/example/repository/PersonRepositoryTest.java
@@ -1,13 +1,18 @@
 package com.johnathanmarksmith.mongodb.example.repository;
 
-import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
-import static org.fest.assertions.Assertions.assertThat;
-
+import com.foursquare.fongo.Fongo;
+import com.johnathanmarksmith.mongodb.example.MongoConfiguration;
+import com.lordofthejars.nosqlunit.annotation.ShouldMatchDataSet;
+import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
+import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
+import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
+import com.mongodb.Mongo;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -16,78 +21,76 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.foursquare.fongo.Fongo;
-import com.lordofthejars.nosqlunit.annotation.ShouldMatchDataSet;
-import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
-import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
-import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
-import com.mongodb.Mongo;
+import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
+import static org.fest.assertions.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
+@ContextConfiguration(classes = {MongoConfiguration.class})
 public class PersonRepositoryTest {
 
-	@Rule
+    @Rule
     public MongoDbRule mongoDbRule = newMongoDbRule().defaultSpringMongoDb("demo-test");
 
-	// nosql-unit requirement
-	@Autowired private ApplicationContext applicationContext;
-	
-	@Autowired private PersonRepository personRepository;
+    // nosql-unit requirement
+    @Autowired private ApplicationContext applicationContext;
 
-	/**
-	 * Expected results are in "one-person.json" file
-	 */
-	@Test
-	@ShouldMatchDataSet(location = "/two-person.json")
-    public void testInsertPersonWithNameJohnathanAndRandomAge(){
-         this.personRepository.insertPersonWithNameJohnathan(35);
-         this.personRepository.insertPersonWithNameJohnathan(67);
-    }
-	
-	/**
-	 * Insert data from "five-person.json" and test countAllPersons method
-	 */
-	@Test
-	@UsingDataSet(locations = {"/five-person.json"}, loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
-    public void testCountAllPersons(){
-         long total = this.personRepository.countAllPersons();
-         
-         assertThat(total).isEqualTo(5);
-    }
-	
-	/**
-	 * Insert data from "five-person.json" and test countUnderAge method
-	 */
-	@Test
-	@UsingDataSet(locations = {"/five-person.json"}, loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
-    public void testCountUnderAge(){
-         long total = this.personRepository.countUnderAge();
-         
-         assertThat(total).isEqualTo(3);
-    }
-	
-	@Configuration
-	@EnableMongoRepositories
-	@ComponentScan(basePackageClasses = {PersonRepository.class})  // modified to not load configs from com.johnathanmarksmith.mongodb.example.MongoConfiguration
-	@PropertySource("classpath:application.properties")
-	static class PersonRepositoryTestConfiguration extends AbstractMongoConfiguration {
+    @Autowired private PersonRepository personRepository;
 
-	    @Override
-	    protected String getDatabaseName() {
-	        return "demo-test";
-	    }
-		
-	    @Override
-		public Mongo mongo() {
-	    	// uses fongo for in-memory tests
-			return new Fongo("mongo-test").getMongo();
-		}
-		
-	    @Override
-	    protected String getMappingBasePackage() {
-	        return "com.johnathanmarksmith.mongodb.example.domain";
-	    }
+    /**
+     * Expected results are in "one-person.json" file
+     */
+    @Test
+    @ShouldMatchDataSet(location = "/two-person.json")
+    public void testInsertPersonWithNameJohnathanAndRandomAge() {
+        this.personRepository.insertPersonWithNameJohnathan(35);
+        this.personRepository.insertPersonWithNameJohnathan(67);
+    }
 
-	}
+    /**
+     * Insert data from "five-person.json" and test countAllPersons method
+     */
+    @Test
+    @UsingDataSet(locations = {"/five-person.json"}, loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    public void testCountAllPersons() {
+        long total = this.personRepository.countAllPersons();
+
+        assertThat(total).isEqualTo(5);
+    }
+
+    /**
+     * Insert data from "five-person.json" and test countUnderAge method
+     */
+    @Test
+    @UsingDataSet(locations = {"/five-person.json"}, loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    public void testCountUnderAge() {
+        long total = this.personRepository.countUnderAge();
+
+        assertThat(total).isEqualTo(3);
+    }
+
+    @Configuration
+    @EnableMongoRepositories
+    @ComponentScan(basePackageClasses = {PersonRepository.class})
+    // modified to not load configs from com.johnathanmarksmith.mongodb.example.MongoConfiguration
+    @PropertySource("classpath:application.properties")
+    static class PersonRepositoryTestConfiguration extends AbstractMongoConfiguration {
+
+        @Override
+        protected String getDatabaseName() {
+            return "demo-test";
+        }
+
+        @Bean
+        @Override
+        public Mongo mongo() {
+            // uses fongo for in-memory tests
+            return new Fongo("mongo-test").getMongo();
+        }
+
+        @Override
+        protected String getMappingBasePackage() {
+            return "com.johnathanmarksmith.mongodb.example.domain";
+        }
+
+    }
 }
